2acb86cb63fef4c0eb524c4537c28713a4ff2f6b
machgo19940122 <machgo122@gmail.com>
2022-09-01 15:45:13 +0900

    http通信

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+                  //heroku
+                  if(\App::environment(['production'])){
+                    \URL::forceScheme('https');
+                  }
     }
 }
